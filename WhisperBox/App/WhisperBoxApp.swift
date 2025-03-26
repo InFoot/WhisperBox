6b6696da6dc606ac422d1bc8180e29a018d56e40
keon22han <keonheehan@hansung.ac.kr>
2025-03-26 16:41:35 +0900

    Feat: Firebase 연동 및 Firebase Realtime DB 데이터 저장, 조회 로직 구현
    - GoogleService-Info.plist 파일 추가
    - Firebase configure() 초기화
    - Firebase 통신 메서드 작성
        - 사용자 계정 추가 (아카데미 사람들 임의 추가하도록, [닉네임, 패스워드])
        - 유저 정보 불러오기 (닉네임, 비밀번호)
        - 사용자 계정 로그인 ([닉네임, 비밀번호])
        - 편지 보내기 (보낸 날짜, 보낸이, 받는이, 익명 여부, 컨텐츠)
        - Date 형식의 날짜 값으로 해당 날 나에게 온 편지 조회
    
    DTO, VO 추후 정리.. ㅎㅎ

--- WhisperBox/App/WhisperBoxApp.swift
+++ WhisperBox/App/WhisperBoxApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct WhisperBoxApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             NavigationStack {
---
+++ WhisperBox/Database/FirebaseController.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseController.swift
+//  WhisperBox
+//
+//  Created by 한건희 on 3/25/25.
+//
+
+import Foundation
+
+class FirebaseController {
+    static let shared = FirebaseController()
+    
+    private init() { }
+    
+//    func saveUserData() async -> Result<Bool, NetworkError> {
+//        await FirebaseService.shared.saveUser()
+//    }
+}
